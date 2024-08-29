c2836f914d1f5ceb2350a58597871d4ab207a284
GourobSaha <gourobsaha009@gmail.com>
2024-08-29 16:01:37 +0600

    env setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestionModule } from './modules/question/question.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost', // If connecting from another machine, replace with the host machine's IP address
-      port: 5432,
-      username: 'root',
-      password: 'gourob',
-      database: 'default_db',
+      host: 'localhost', // Adjust if connecting from another machine
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Set to false in production
+      synchronize: true,
     }),
     QuestionModule,
   ],
